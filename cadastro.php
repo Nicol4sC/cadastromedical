3801689ca569bf31a49c3fe5934f88c74fcba8dd
Nicolas Cardozo <125682448+Nicol4sC@users.noreply.github.com>
2023-10-07 12:42:25 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div class="cad">
+        <h1>CADASTRAR</h1>
+        <form method="POST" action="envia.php">
+            <label>Nome:</label>
+            <input type="text" name="nome" placeholder="Digite seu nome completo"><br><br>
+
+            <label>Email:</label>
+            <input type="email" name="email" placeholder="Digite seu email"><br><br>
+
+            <input type="submit" value="Cadastrar">
+
+        </form>
+    </div>
+    
+</body>
+</html>
